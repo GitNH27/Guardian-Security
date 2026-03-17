68042b6334e82c69e28abbf5adcaf0d6f8498af2
GitNH27 <nicholas.hum27@gmail.com>
2026-03-16 22:41:42 -0400

    .withSockJs

--- security-backend/src/main/java/com/GuardianSecurity/security_backend/config/WebSocketConfig.java
+++ security-backend/src/main/java/com/GuardianSecurity/security_backend/config/WebSocketConfig.java
@@ -4,9 +4,6 @@ import com.GuardianSecurity.security_backend.security.JwtSecurityTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -16,11 +13,10 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import org.springframework.security.authorization.AuthorizationManager;
-// import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -93,6 +89,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-security")
-                .setAllowedOriginPatterns("*"); // Removed SockJS for direct WebSocket connection
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
